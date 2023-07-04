9864fa99478a0db46c58e00641de15b8cb2e5b9c
Nicholas <nicholas@xx.oo>
2023-07-04 08:26:45 +0800

    feat: 导出模块

--- src/PersistContainer/index.ts
+++ src/PersistContainer/index.ts
@@ -1,12 +1,1 @@
-import { PersistContainer, PersistContainerProps, Field, FieldType, FieldGroup, PageOptions, renderField, renderFieldGroup } from './PersistContainer';
-
-export {
-  PersistContainer,
-  PersistContainerProps,
-  Field,
-  FieldType,
-  FieldGroup,
-  PageOptions,
-  renderField,
-  renderFieldGroup
-};
+export * from './PersistContainer'
